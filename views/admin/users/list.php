d99e909e0cb9da65be2923bf849e4a14a22dea13
NaysKutzu <polargaiming@yahoo.com>
2023-09-09 19:53:16 +0000

    PUSH -> Updated theme for the register page

--- views/admin/users/list.php
+++ views/admin/users/list.php
@@ -78,16 +78,6 @@ $totalPages = ceil($totalUsers / $usersPerPage);
                                                                 <i class="ki-outline ki-plus fs-2"></i>Add User
                                                             </button>
                                                         </div>
-                                                        <div class="d-flex justify-content-end align-items-center d-none"
-                                                            data-kt-user-table-toolbar="selected">
-                                                            <div class="fw-bold me-5">
-                                                                <span class="me-2"
-                                                                    data-kt-user-table-select="selected_count"></span>Selected
-                                                            </div>
-                                                            <button type="button" class="btn btn-danger"
-                                                                data-kt-user-table-select="delete_selected">Delete
-                                                                Selected</button>
-                                                        </div>
                                                     </div>
                                                 </div>
                                                 <div class="card-body py-4">
